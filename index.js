90726074839fb409905f11ece0c0fe2641eb4b2c
botto54 <bryanlbottorff@gmail.com>
2020-11-24 00:58:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+// Test 1 passes with and without the following constants
+
+// const arrayOfNames = ['Lisa', 'Kaitlin', 'Jan']
+// const eventNames = 'surpe'
+
+function writeCards(arrayOfNames, eventNames) {
+    let thankYouCards = []
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        thankYouCards.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventNames} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(start){
+    while (start > 0) {
+        console.log(start);
+        start -= 1;
+    }
+    console.log(start)
+}
